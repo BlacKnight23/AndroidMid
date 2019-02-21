44aafffa4d6556a89574d92576206c8cbb1cc8be
mendozarain <34019684+mendozarain@users.noreply.github.com>
2019-02-21 22:31:50 +0800

    F7_mid

---
+++ android1.1/platforms/browser/www/js/my-app.js
@@ -0,0 +1,82 @@
+// Initialize app
+var myApp = new Framework7();
+  
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+      {
+        path: '/details/',
+        url: 'details.html',
+      },
+      {
+        path: '/place/',
+        url: 'locate.html',
+      },
+      {
+        path: '/map/',
+        url: 'map.html',
+      },
+      {
+        path: '/who/',
+        url: 'who.html',
+      },
+
+    ]
+    // ... other parameters
+  });
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+})
+
+
+
+
+var myPhotoBrowserDark = app.photoBrowser.create({
+    photos : [
+        'pages/images/menu.png',
+    ],
+    theme: 'dark'
+});
+
+function pb(){
+
+myPhotoBrowserDark.open();
+
+
+
+}
+
+
